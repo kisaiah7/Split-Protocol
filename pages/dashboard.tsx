cd5b9c681de0cfa878213216c436a596817bb464
Yangha Kim <yangha.kim.96@gmail.com>
2022-07-13 17:47:28 -0400

    auto margin for dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,7 +4,7 @@ import Preview from '../components/preview'
 const Dashboard: NextPage = () => {
     return (
         <div className='h-full bg-body-gradient pb-20'>
-            <div className='container'>
+            <div className='container mx-auto'>
                 <p className='pt-16 pb-8 text-primary text-3xl font-sans font-bold'>Your Expenses</p>
                 <div className='grid grid-cols-4 gap-4'>
                     <Preview />
