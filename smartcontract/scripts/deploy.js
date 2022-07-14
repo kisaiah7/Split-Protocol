afbcbae07703274dedd0cfd55c66a41a6267965c
narteysarso <narteysarso@gmail.com>
2022-07-14 18:16:44 +0000

    Add bare deploy script

--- smartcontract/scripts/deploy.js
+++ smartcontract/scripts/deploy.js
@@ -1,29 +1,31 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const {ethers} = require('hardhat');
+require("dotenv").config({path: ".env"});
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+function main(){
+  //contract deployments
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// auto write contract abi.json after deployment
+const storeContractData = (contract, contractName) => {
+  const fs = require("fs");
+  const contractDir = `${__dirname}/../abis`;
+
+  if (!fs.existsSync(contractDir)) {
+    fs.mkdirSync(contractDir);
+  }
+
+  const contractArtiacts = artifacts.readArtifactSync(contractName);
+
+  fs.writeFileSync(
+    contractDir + `/${contractName}.json`,
+    JSON.stringify({ address: contract.address, ...contractArtiacts }, null, 2)
+  );
+};
+
+
+main().then( () => process.exit(0))
+.catch( error => {
+  console.log(error);
+  process.exit(1);
+})
