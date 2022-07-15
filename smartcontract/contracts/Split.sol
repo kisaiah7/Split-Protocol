f9929e106f5ee887dcfeab30355791252f73850e
narteysarso <narteysarso@gmail.com>
2022-07-15 12:55:45 +0000

    add create expense, get expenses functionalities and tests

--- smartcontract/contracts/Split.sol
+++ smartcontract/contracts/Split.sol
@@ -3,12 +3,29 @@
 pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Split is Ownable{
+contract Split is Ownable {
+    using Counters for Counters.Counter;
 
-    event ExpenseCreated(uint indexed index, address indexed creator, address indexed recipient, uint amount);
-    event DebtorPaid(uint indexed index, address indexed recipient, address indexed debtor, uint amount);
-    event ExpenseCancelled(uint indexed index, address indexed creator, address indexed recipient, uint amount);
+    event ExpenseCreated(
+        uint indexed index,
+        address indexed creator,
+        address indexed recipient,
+        uint amount
+    );
+    event DebtorPaid(
+        uint indexed index,
+        address indexed recipient,
+        address indexed debtor,
+        uint amount
+    );
+    event ExpenseCancelled(
+        uint indexed index,
+        address indexed creator,
+        address indexed recipient,
+        uint amount
+    );
 
     //pause contract during emergency
     bool _pauseContract = false;
@@ -19,31 +36,25 @@ contract Split is Ownable{
     // mapping debtor address to expense id
     mapping(address => mapping(uint => uint)) _debtorExpenses;
 
-    // mapping recipient address to expense id
-    mapping(address => mapping (uint => uint)) _recipientExpenses;
-
     // mapping expense id to creator id
-    mapping(uint => address ) _expenseCreator;
-
-    // mapping expense id to recipient id
-    mapping(uint => address) _expenseRecipient;
-    
-    // mapping expense id to debtor id
-    mapping(uint => address) _expenseDebtor;
+    mapping(uint => address) _expenseCreator;
 
     // mapping number of creator expenses;
     mapping(address => uint) _createdExpenseOf;
 
-    // mapping number of recipient expenses;
-    mapping(address => uint) _receivingExpenseOf;
-
     // mapping number of debtor expenses;
     mapping(address => uint) _owedExpenseOf;
+
+    // All Expense
+    mapping(uint => Expense) _allExpenses;
+
+    Counters.Counter _expenseIndex;
+
 
     enum ExpenseStatus {
         PENDING,
         PAID,
-        CANCELLED  
+        CANCELLED
     }
 
     enum ExpenseCategory {
@@ -53,20 +64,22 @@ contract Split is Ownable{
         MISC
     }
 
-    struct Participant{
+    struct Participant {
+        address _address;
         string name;
         string avatarURL;
     }
 
     struct Debtor {
+        address _address;
         string name;
         string avatarURL;
         uint amount;
-        bool hasPayed;
-        uint paidAt;
+        // bool hasPayed;
+        // uint paidAt;
     }
 
-    struct Expense{
+    struct Expense {
         string name;
         string description;
         ExpenseCategory category;
@@ -80,27 +93,170 @@ contract Split is Ownable{
         Debtor[] debtors;
     }
 
-    // All Expense
-    Expense[] _expenses; 
-
-    modifier onlyCreator(uint expenseIndex){
-        require(_expenseCreator[expenseIndex] == msg.sender, "Access denied. Only creator");
+    modifier creatorOf(uint expenseIndex) {
+        require(
+            _expenseCreator[expenseIndex] == msg.sender,
+            "Access denied. Only creator"
+        );
         _;
     }
 
-    modifier onlyRecipient(uint expenseIndex){
-        require(_expenseRecipient[expenseIndex] == msg.sender,"Access denied. Only recipient");
-        _;
-    }
-
-    modifier onlyDebtor(uint expenseIndex){
-        require(_expenseDebtor[expenseIndex] == msg.sender, "Access denied. Only debtor");
-        _;
-    }
-
-    modifier notPaused {
+    modifier notPaused() {
         require(!_pauseContract, "Contract is paused");
         _;
+    }
+
+    function createExpense(
+        string memory _name,
+        string memory _description,
+        uint _amount,
+        address _tokenAddress,
+        ExpenseCategory _category,
+        uint _paymentDue,
+        Participant memory _recipient,
+        Participant memory _creator,
+        Debtor[] memory _debtors
+    ) public notPaused {
+        //data validation
+        require(bytes(_name).length > 0, "Expense name is required");
+        require(_amount > 0, "amount must be greater than 0");
+        require(_tokenAddress != address(0), "Invalid token address or ENS names");
+        require(_recipient._address != address(0), "Invalid  recipient address or ENS names");
+
+        uint expenseIndex = _expenseIndex.current();
+
+        //create new Expense
+        Expense storage expense = _allExpenses[expenseIndex];
+        expense.name = _name;
+        expense.description = _description;
+        expense.amount = _amount;
+        expense.token = _tokenAddress;
+        expense.recipient = _recipient;
+        expense.creator = _creator;
+        expense.createdAt = block.timestamp;
+        expense.category = _category;
+        expense.status = ExpenseStatus.PENDING;
+        expense.paymentDue = _paymentDue;
+
+        address creatorAddress = _creator._address;
+
+        // get number of expenses created by `_creator`
+        uint numberOfCreatorExpenses = _createdExpenseOf[creatorAddress];
+
+        // increase the creators number of created expenses
+        _createdExpenseOf[creatorAddress] += 1;
+
+        // assign expense index to creator
+        _creatorExpenses[creatorAddress][
+            numberOfCreatorExpenses
+        ] = expenseIndex;
+
+        // Implicit memory to storage conversion is not supported
+        // so we do it manually
+        for (uint idx; idx < _debtors.length; idx++) {
+            address debtorAddress = _debtors[idx]._address;
+
+            require(debtorAddress != address(0), "invalid address debtor or ENS name");
+
+            //get number of expense of debtor
+            uint numberOfOwedExpense = _owedExpenseOf[debtorAddress];
+
+            // increase the number of debtors owed expenses;
+            _owedExpenseOf[debtorAddress] += 1;
+
+            // assign expense index to debtor
+            _debtorExpenses[debtorAddress][numberOfOwedExpense] = expenseIndex;
+
+            // append debtor expense list of debtors
+            expense.debtors.push(_debtors[idx]);
+        }
+
+        _expenseIndex.increment();
+
+        emit ExpenseCreated(
+            expenseIndex,
+            _creator._address,
+            _recipient._address,
+            _amount
+        );
+    }
+
+    function getNumberOfCreatedExpenses(address _creatorAddress)
+        external
+        view
+        notPaused
+        returns (uint)
+    {
+        require(_creatorAddress != address(0), "Invalid address");
+        return _createdExpenseOf[_creatorAddress];
+    }
+
+    function getNumberOfOwedExpenses(address _debtorAddress)
+        external
+        view
+        notPaused
+        returns (uint)
+    {
+        require(_debtorAddress != address(0), "Invalid address");
+        return _owedExpenseOf[_debtorAddress];
+    }
+
+    // get
+    function getCreatedExpense(address _creatorAddress, uint index)
+        external
+        view
+        notPaused
+        returns (
+            string memory,
+            string memory,
+            ExpenseCategory,
+            uint,
+            address,
+            ExpenseStatus,
+            uint,
+            uint
+        )
+    {
+        uint expenseIndex = _creatorExpenses[_creatorAddress][index];
+        Expense storage expense = _allExpenses[expenseIndex];
+
+        return (
+            expense.name,
+            expense.description,
+            expense.category,
+            expense.amount,
+            expense.token,
+            expense.status,
+            expense.paymentDue,
+            expense.createdAt
+        );
+    }
+
+    // get debt of address detail
+    function getDebt(address _debtorAddress, uint index)
+        external
+        view
+        notPaused
+        returns (
+            string memory,
+            string memory,
+            ExpenseCategory,
+            ExpenseStatus,
+            uint,
+            uint
+        )
+    {
+        uint expenseIndex = _debtorExpenses[_debtorAddress][index];
+        Expense storage expense = _allExpenses[expenseIndex];
+
+        return (
+            expense.name,
+            expense.description,
+            expense.category,
+            expense.status,
+            expense.paymentDue,
+            expense.createdAt
+        );
     }
 
     function pauseContract(bool status) external onlyOwner {
---
+++ smartcontract/test/split.test.js
@@ -0,0 +1,407 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const WETH9 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+const TWENTY_FOUR_HRS = 24 * 60 * 60 * 1000;
+
+const ExpenseCategory = {
+    ACCOMODATION: 0,
+    TRANSPORTATION: 1,
+    FOOD: 2,
+    MISC: 3,
+};
+
+describe("Split Contract", function () {
+    let SplitContract;
+    let owner;
+    let issuerContract;
+    let creator;
+    let recipient;
+    let debtor1;
+    let debtor2;
+    let debtor3;
+    let debtor4;
+    let debtor5;
+
+    beforeEach(async function () {
+        [
+            owner,
+            issuerContract,
+            recipient,
+            creator,
+            debtor1,
+            debtor2,
+            debtor3,
+            debtor4,
+            debtor5,
+        ] = await ethers.getSigners();
+        const contract = await ethers.getContractFactory("Split");
+        SplitContract = await contract.deploy();
+
+        await SplitContract.deployed();
+    });
+
+    it("Should create an expense", async function () {
+        const expenseObj = {
+            name: "Test Bar expense",
+            description: "Spliting expenses among 5 people",
+            amount: 1000,
+            tokenAddress: DAI,
+            paymentDue: Date.now() + TWENTY_FOUR_HRS,
+            category: ExpenseCategory.ACCOMODATION,
+            recipient: {
+                _address: recipient.address,
+                name: "recipient",
+                avatarURL: "",
+            },
+            creator: {
+                _address: creator.address,
+                name: "creator",
+                avatarURL: "",
+            },
+            debtors: [
+                {
+                    _address: debtor1.address,
+                    amount: 200,
+                    tokenAddress: DAI,
+                    name: "debtor1",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor2.address,
+                    amount: 200,
+                    tokenAddress: WETH9,
+                    name: "debtor2",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor3.address,
+                    amount: 200,
+                    tokenAddress: USDC,
+                    name: "debtor3",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor4.address,
+                    amount: 200,
+                    tokenAddress: DAI,
+                    name: "debtor4",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor5.address,
+                    amount: 200,
+                    tokenAddress: WETH9,
+                    name: "debtor5",
+                    avatarURL: "",
+                },
+            ],
+        };
+
+        await SplitContract.connect(creator).createExpense(
+            expenseObj.name,
+            expenseObj.description,
+            expenseObj.amount,
+            expenseObj.tokenAddress,
+            expenseObj.category,
+            expenseObj.paymentDue,
+            expenseObj.recipient,
+            expenseObj.creator,
+            expenseObj.debtors
+        );
+
+        expect(
+            await SplitContract.getNumberOfCreatedExpenses(creator.address)
+        ).to.equal(1);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor1.address)
+        ).to.equal(1);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor2.address)
+        ).to.equal(1);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor3.address)
+        ).to.equal(1);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor4.address)
+        ).to.equal(1);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor5.address)
+        ).to.equal(1);
+    });
+
+    it("Should revert expense creation with 'Expense name is required'", async function () {
+        const expenseObj = {
+            name: "",
+            description: "Spliting expenses among 5 people",
+            amount: 1000,
+            tokenAddress: DAI,
+            paymentDue: Date.now() + TWENTY_FOUR_HRS,
+            category: ExpenseCategory.ACCOMODATION,
+            recipient: {
+                _address: recipient.address,
+                name: "recipient",
+                avatarURL: "",
+            },
+            creator: {
+                _address: creator.address,
+                name: "creator",
+                avatarURL: "",
+            },
+            debtors: [
+                {
+                    _address: debtor1.address,
+                    amount: 200,
+                    tokenAddress: DAI,
+                    name: "debtor1",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor2.address,
+                    amount: 200,
+                    tokenAddress: WETH9,
+                    name: "debtor2",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor3.address,
+                    amount: 200,
+                    tokenAddress: USDC,
+                    name: "debtor3",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor4.address,
+                    amount: 200,
+                    tokenAddress: DAI,
+                    name: "debtor4",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor5.address,
+                    amount: 200,
+                    tokenAddress: WETH9,
+                    name: "debtor5",
+                    avatarURL: "",
+                },
+            ],
+        };
+
+        await expect(
+            SplitContract.connect(creator).createExpense(
+                expenseObj.name,
+                expenseObj.description,
+                expenseObj.amount,
+                expenseObj.tokenAddress,
+                expenseObj.category,
+                expenseObj.paymentDue,
+                expenseObj.recipient,
+                expenseObj.creator,
+                expenseObj.debtors
+            )
+        ).to.revertedWith("Expense name is required");
+
+        expect(
+            await SplitContract.getNumberOfCreatedExpenses(creator.address)
+        ).to.equal(0);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor1.address)
+        ).to.equal(0);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor2.address)
+        ).to.equal(0);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor3.address)
+        ).to.equal(0);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor4.address)
+        ).to.equal(0);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor5.address)
+        ).to.equal(0);
+    });
+
+    it("Should revert expense creation with 'amount must be greater than 0'", async function () {
+        const expenseObj = {
+            name: "Test Bar expense",
+            description: "Spliting expenses among 5 people",
+            amount: 0,
+            tokenAddress: DAI,
+            paymentDue: Date.now() + TWENTY_FOUR_HRS,
+            category: ExpenseCategory.ACCOMODATION,
+            recipient: {
+                _address: recipient.address,
+                name: "recipient",
+                avatarURL: "",
+            },
+            creator: {
+                _address: creator.address,
+                name: "creator",
+                avatarURL: "",
+            },
+            debtors: [
+                {
+                    _address: debtor1.address,
+                    amount: 200,
+                    tokenAddress: DAI,
+                    name: "debtor1",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor2.address,
+                    amount: 200,
+                    tokenAddress: WETH9,
+                    name: "debtor2",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor3.address,
+                    amount: 200,
+                    tokenAddress: USDC,
+                    name: "debtor3",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor4.address,
+                    amount: 200,
+                    tokenAddress: DAI,
+                    name: "debtor4",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor5.address,
+                    amount: 200,
+                    tokenAddress: WETH9,
+                    name: "debtor5",
+                    avatarURL: "",
+                },
+            ],
+        };
+
+        await expect(
+            SplitContract.connect(creator).createExpense(
+                expenseObj.name,
+                expenseObj.description,
+                expenseObj.amount,
+                expenseObj.tokenAddress,
+                expenseObj.category,
+                expenseObj.paymentDue,
+                expenseObj.recipient,
+                expenseObj.creator,
+                expenseObj.debtors
+            )
+        ).to.revertedWith("amount must be greater than 0");
+
+        expect(
+            await SplitContract.getNumberOfCreatedExpenses(creator.address)
+        ).to.equal(0);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor1.address)
+        ).to.equal(0);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor2.address)
+        ).to.equal(0);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor3.address)
+        ).to.equal(0);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor4.address)
+        ).to.equal(0);
+        expect(
+            await SplitContract.getNumberOfOwedExpenses(debtor5.address)
+        ).to.equal(0);
+    });
+
+    it("should get created expenses", async function () {
+        const expenseObj = {
+            name: "Test Bar expense",
+            description: "Spliting expenses among 5 people",
+            amount: 1000,
+            tokenAddress: DAI,
+            paymentDue: Date.now() + TWENTY_FOUR_HRS,
+            category: ExpenseCategory.ACCOMODATION,
+            recipient: {
+                _address: recipient.address,
+                name: "recipient",
+                avatarURL: "",
+            },
+            creator: {
+                _address: creator.address,
+                name: "creator",
+                avatarURL: "",
+            },
+            debtors: [
+                {
+                    _address: debtor1.address,
+                    amount: 200,
+                    tokenAddress: DAI,
+                    name: "debtor1",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor2.address,
+                    amount: 200,
+                    tokenAddress: WETH9,
+                    name: "debtor2",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor3.address,
+                    amount: 200,
+                    tokenAddress: USDC,
+                    name: "debtor3",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor4.address,
+                    amount: 200,
+                    tokenAddress: DAI,
+                    name: "debtor4",
+                    avatarURL: "",
+                },
+                {
+                    _address: debtor5.address,
+                    amount: 200,
+                    tokenAddress: WETH9,
+                    name: "debtor5",
+                    avatarURL: "",
+                },
+            ],
+        };
+
+        await SplitContract.connect(creator).createExpense(
+            expenseObj.name,
+            expenseObj.description,
+            expenseObj.amount,
+            expenseObj.tokenAddress,
+            expenseObj.category,
+            expenseObj.paymentDue,
+            expenseObj.recipient,
+            expenseObj.creator,
+            expenseObj.debtors
+        );
+
+        const numberOfCreatedExpenses =
+            await SplitContract.getNumberOfCreatedExpenses(creator.address);
+
+        for (let idx = 0; idx < numberOfCreatedExpenses; idx++) {
+            result = await SplitContract.getCreatedExpense(creator.address, 0);
+
+            expect(result).to.include("Test Bar expense");
+            expect(result).to.include("Spliting expenses among 5 people");
+            expect(result).to.include(DAI);
+        }
+    });
+
+    it("Should puase contract", async function () {
+        await SplitContract.connect(owner).pauseContract(true);
+    });
+
+    it("Should revert with 'Ownable: caller is not the owner' ", async function () {
+        await expect(
+            SplitContract.connect(issuerContract).pauseContract(true)
+        ).to.revertedWith("Ownable: caller is not the owner");
+    });
+});
