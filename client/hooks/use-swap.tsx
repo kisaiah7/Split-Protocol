982c36e26e31bb448d8a6d4cddd0179f18c8c706
Jonas Wolfram <jonas-wolfram@web.de>
2022-07-22 17:17:42 +0700

    Included uniswap pool logic

---
+++ client/hooks/use-swap.tsx
@@ -0,0 +1,254 @@
+import { ethers } from 'ethers';
+import { Pool } from '@uniswap/v3-sdk';
+import { Token } from '@uniswap/sdk-core';
+import IUniswapV3PoolArtifact from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json';
+import { useContract, useProvider } from 'wagmi';
+import { PoolImmutables } from '../models/pool-immutables';
+import { PoolState } from '../models/pool-state';
+import ERC20ABI from '../utils/abis/ERC20.json';
+import { TokenImmutables } from '../models/token-immutables';
+import { polygonMumbai } from 'wagmi/chains';
+import { useRef, useState } from 'react';
+import QuoterArtifact from '@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json';
+import { TokenSymbol } from '../enums/TokenSymbol';
+import { formatUnits, parseUnits } from '../utils/token-helper';
+
+const USDC_WETH_POOL_CONTRACT_ADDRESS =
+  '0x45dda9cb7c25131df268515131f647d726f50608';
+const MATIC_WETH_POOL_CONTRACT_ADDRESS =
+  '0x167384319b41f7094e62f7506409eb38079abff8';
+const MATIC_USDC_POOL_CONTRACT_ADDRESS =
+  '0xa374094527e1673a86de625aa59517c5de346d32';
+const QUOTER_CONTRACT_ADDRESS = '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6';
+
+const pools: {
+  tokenSymbolPair: [string, string];
+  poolContractAddress: string;
+}[] = [
+  {
+    tokenSymbolPair: [TokenSymbol.USDC, TokenSymbol.WETH],
+    poolContractAddress: USDC_WETH_POOL_CONTRACT_ADDRESS,
+  },
+  {
+    tokenSymbolPair: [TokenSymbol.WMATIC, TokenSymbol.WETH],
+    poolContractAddress: MATIC_WETH_POOL_CONTRACT_ADDRESS,
+  },
+  {
+    tokenSymbolPair: [TokenSymbol.WMATIC, TokenSymbol.USDC],
+    poolContractAddress: MATIC_USDC_POOL_CONTRACT_ADDRESS,
+  },
+];
+
+const useSwap = () => {
+  const provider = useProvider();
+  const quoterContract = useContract({
+    addressOrName: QUOTER_CONTRACT_ADDRESS,
+    contractInterface: QuoterArtifact.abi,
+    signerOrProvider: provider,
+  });
+
+  const [fromTokenPrice, setFromTokenPrice] = useState<string | null>(null);
+  const [toTokenPrice, setToTokenPrice] = useState<string | null>(null);
+
+  const fromTokenRef = useRef<Token | null>(null);
+  const toTokenRef = useRef<Token | null>(null);
+  const poolRef = useRef<Pool | null>(null);
+
+  const changePool = async (fromTokenSymbol: string, toTokenSymbol: string) => {
+    try {
+      const poolContractAddress = getPoolContractAddress(
+        fromTokenSymbol,
+        toTokenSymbol
+      );
+      const poolContract = createPoolContract(poolContractAddress);
+
+      const [immutables, state] = await Promise.all([
+        getPoolImmutables(poolContract),
+        getPoolState(poolContract),
+      ]);
+
+      const token0Contract = createTokenContract(immutables.token0);
+      const token1Contract = createTokenContract(immutables.token1);
+
+      const [token0, token1] = await Promise.all([
+        createToken(token0Contract),
+        createToken(token1Contract),
+      ]);
+
+      const pool = new Pool(
+        token0,
+        token1,
+        immutables.fee,
+        state.sqrtPriceX96.toString(),
+        state.liquidity.toString(),
+        state.tick
+      );
+      poolRef.current = pool;
+
+      let fromTokenPrice, toTokenPrice;
+
+      if (fromTokenSymbol === token0.symbol) {
+        fromTokenRef.current = token0;
+        toTokenRef.current = token1;
+        fromTokenPrice = pool.token0Price.toFixed(token1.decimals);
+        toTokenPrice = pool.token1Price.toFixed(token0.decimals);
+      } else {
+        fromTokenRef.current = token1;
+        toTokenRef.current = token0;
+        fromTokenPrice = pool.token1Price.toFixed(token0.decimals);
+        toTokenPrice = pool.token0Price.toFixed(token1.decimals);
+      }
+
+      setFromTokenPrice(fromTokenPrice);
+      setToTokenPrice(toTokenPrice);
+    } catch (err) {
+      console.error('Error trying to initialize a pool', err);
+    }
+  };
+
+  const getPoolContractAddress = (
+    fromTokenSymbol: string,
+    toTokenSymbol: string
+  ) => {
+    const pool = pools.find(
+      (pool) =>
+        pool.tokenSymbolPair.includes(fromTokenSymbol) &&
+        pool.tokenSymbolPair.includes(toTokenSymbol)
+    );
+    if (!pool)
+      throw new Error('No pool could be found for the selected tokens');
+    return pool.poolContractAddress;
+  };
+
+  const createPoolContract = (poolContractAddress: string) => {
+    const poolContract = new ethers.Contract(
+      poolContractAddress,
+      IUniswapV3PoolArtifact.abi,
+      provider
+    );
+    return poolContract;
+  };
+
+  const getPoolImmutables = async (poolContract: ethers.Contract) => {
+    const [factory, token0, token1, fee, tickSpacing, maxLiquidityPerTick] =
+      await Promise.all([
+        poolContract.factory(),
+        poolContract.token0(),
+        poolContract.token1(),
+        poolContract.fee(),
+        poolContract.tickSpacing(),
+        poolContract.maxLiquidityPerTick(),
+      ]);
+
+    const poolImmutables: PoolImmutables = {
+      factory,
+      token0,
+      token1,
+      fee,
+      tickSpacing,
+      maxLiquidityPerTick,
+    };
+    return poolImmutables;
+  };
+
+  const getPoolState = async (poolContract: ethers.Contract) => {
+    const [liquidity, slot] = await Promise.all([
+      poolContract.liquidity(),
+      poolContract.slot0(),
+    ]);
+
+    const PoolState: PoolState = {
+      liquidity,
+      sqrtPriceX96: slot[0],
+      tick: slot[1],
+      observationIndex: slot[2],
+      observationCardinality: slot[3],
+      observationCardinalityNext: slot[4],
+      feeProtocol: slot[5],
+      unlocked: slot[6],
+    };
+
+    return PoolState;
+  };
+
+  const createTokenContract = (tokenContractAddress: string) => {
+    const tokenContract = new ethers.Contract(
+      tokenContractAddress,
+      ERC20ABI as any,
+      provider
+    );
+    return tokenContract;
+  };
+
+  const createToken = async (
+    tokenContract: ethers.Contract,
+    chain = polygonMumbai
+  ) => {
+    const tokenImmutables = await getTokenImmutables(tokenContract);
+    console.log(tokenImmutables);
+    const token = new Token(
+      chain.id,
+      tokenContract.address,
+      tokenImmutables.decimals,
+      tokenImmutables.symbol,
+      tokenImmutables.name
+    );
+    return token;
+  };
+
+  const getTokenImmutables = async (tokenContract: ethers.Contract) => {
+    const [decimals, symbol, name] = await Promise.all([
+      tokenContract.decimals(),
+      tokenContract.symbol(),
+      tokenContract.name(),
+    ]);
+
+    const tokenImmutables: TokenImmutables = {
+      decimals,
+      symbol,
+      name,
+    };
+
+    return tokenImmutables;
+  };
+
+  const getQuoteReverse = async (amountOut: number) => {
+    try {
+      const pool = poolRef.current;
+      if (!pool) throw new Error('pool is undefined');
+
+      const fromToken = fromTokenRef.current;
+      if (!fromToken) throw new Error('fromToken is undefined');
+
+      const toToken = toTokenRef.current;
+      if (!toToken) throw new Error('toToken is undefined');
+
+      const parsedAmountOut = parseUnits(amountOut, toToken.decimals);
+
+      const amountIn = await quoterContract.callStatic.quoteExactOutputSingle(
+        fromToken.address,
+        toToken.address,
+        pool.fee,
+        parsedAmountOut.toString(),
+        0
+      );
+
+      const formattedAmountIn = formatUnits(amountIn, fromToken.decimals);
+      return formattedAmountIn;
+    } catch (err) {
+      console.error('Error trying to get a quote', err);
+      throw err;
+    }
+  };
+
+  return {
+    fromToken: fromTokenRef.current,
+    toToken: toTokenRef.current,
+    pool: poolRef.current,
+    fromTokenPrice,
+    toTokenPrice,
+    changePool,
+    getQuoteReverse,
+  };
+};
+export default useSwap;
---
+++ client/models/pool-immutables.tsx
@@ -0,0 +1,10 @@
+import { ethers } from 'ethers';
+
+export interface PoolImmutables {
+  factory: string;
+  token0: string;
+  token1: string;
+  fee: number;
+  tickSpacing: number;
+  maxLiquidityPerTick: ethers.BigNumber;
+}
