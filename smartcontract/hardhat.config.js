308cefe61ec4dfa966ad0478e13dc95f6bc047a9
Peter Sparacino <72105234+psparacino@users.noreply.github.com>
2022-07-22 14:55:44 -0400

    console commands

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -1,6 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config({path: ".env"});
 
+// import '@nomiclabs/hardhat-ethers'
+// import '@nomiclabs/hardhat-waffle'
+// import '@nomiclabs/hardhat-etherscan'
+// import { task } from "hardhat/config"
+// import { BigNumber } from "ethers"
+// import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+
+// task("accounts", "Prints the list of accounts", async (args, hre): Promise<void> => {
+//   const accounts: SignerWithAddress[] = await hre.ethers.getSigners()
+//   accounts.forEach((account: SignerWithAddress): void => {
+//     console.log(account.address)
+//   })
+// })
+
+// task("balances", "Prints the list of AVAX account balances", async (args, hre): Promise<void> => {
+//   const accounts: SignerWithAddress[] = await hre.ethers.getSigners()
+//   for(const account of accounts){
+//     const balance: BigNumber = await hre.ethers.provider.getBalance(
+//       account.address
+//     );
+//     console.log(`${account.address} has balance ${balance.toString()}`);
+//   }
+// })
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
@@ -19,3 +43,6 @@ module.exports = {
     }
   }
 };
+
+
+
--- subgraph/temp/triggerEvents.js
+++ subgraph/temp/triggerEvents.js
@@ -2,6 +2,235 @@
 // const accounts = await hre.ethers.getSigners()
 
 // FOR CONSOLE: npx hardhat console --network localhost
+const abi = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_spender",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "name": "balance",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            },
+            {
+                "name": "_spender",
+                "type": "address"
+            }
+        ],
+        "name": "allowance",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "fallback"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "owner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Approval",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    }
+]
+
+let signer = await ethers.provider.getSigner()
+
+const wmatic20 = new ethers.Contract('0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', abi, signer);
+
+const wmatic = await ethers.getContractAt(abi,'0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', signer);
+
 const Split = await ethers.getContractFactory('Split');
 const split = await Split.attach('0xC10DFcc06b37660446cB99a7A2800469F8D58fE5')
 
@@ -18,5 +247,16 @@ const expense = await split.createExpense(
     ['0x2559fF0F61870134a1d75cE3F271878DCDb0eEE1'],
     ['0x6ada131794388B83c2b3e5b8A24A4677b3aD2E39'],
     [['0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',100000000]])
+
+
+
+//     const Factory = await ethers.getContractFactory('UniswapV3Factory');
+// const Factory1 = await Factory.attach('0x1F98431c8aD98523631AE4a59f267346ea31F984')
+
+// let signer = await ethers.provider.getSigner()
+
+// // const Factory1 = await ethers.getContractAt('UniswapV3Factory','0x1F98431c8aD98523631AE4a59f267346ea31F984', signer);
+
+// let factory = Factory1.connect(signer)
 
 
